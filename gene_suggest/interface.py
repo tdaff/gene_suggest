7b5a1f9f521fc043bedad510b390c1e18bfd6b7f
Tom Daff <tdd20@cam.ac.uk>
2017-12-10 17:08:17 +0000

    Implement interface to database in sqlalchemy

--- gene_suggest/interface.py
+++ gene_suggest/interface.py
@@ -4,6 +4,118 @@ data at the source.
 
 """
 
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy import Table, MetaData
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# The default ENSEMBL database
+DEFAULT_URL = ('mysql+pymysql://anonymous@ensembldb.ensembl.org:3306/'
+               'ensembl_website_90')
+
+
+def get_connection(url=None, env_var='DBURL'):
+    """Create a connection to the sql database with sqlalchemy.
+
+    Either pass the URL of the database to connect to, or it will be taken
+    from an environment variable. If the environment variable is empty then
+    fallback to a default
+
+    Parameters
+    ----------
+    url : str or None
+        sqlalchemy style URL that follows RFC-1738 and encodes all the
+        information about the database. Has the form:
+            dialect+driver://user:pass@host:port/database
+        e.g.
+            mysql+pymysql://anonymous@dbexample.org:1234/main_dbase
+        If url is None, the URL is pulled from an environment variable.
+    env_var : str or None
+        Name of the environment variable to check for a url if none
+        has been passed as the url argument.
+
+    Returns
+    -------
+    engine : Engine
+        The database engine initialised with the url.
+    meta : MetaData
+        The metadata associated with the database.
+    session : scoped_session
+        A session to interact with the database.
+    """
+
+    # Grab a url from the environment if not specified explicitly
+    if url is None:
+        url = os.getenv(env_var, DEFAULT_URL)
+
+    # Use sqlalchemy to interact with the database
+    # The ORM adds overhead but allows for fast prototyping
+    # and makes expanding the scope much easier than dealing
+    # with handwritten sql.
+    # More importantly, it is much safer than trying to escape
+    # user data manually!
+    Session = scoped_session(sessionmaker())
+    engine = create_engine(url, pool_recycle=360)  #, echo=True)  # show sql
+    meta = MetaData(bind=engine)
+    Session.configure(bind=engine)
+    session = Session()
+
+    # enough parts to interact with the database
+    return engine, meta, session
+
 
 def get_suggestions(query, species=None, limit=None):
-    return ['BRCA1', 'BRCA2', 'BRCC3', 'BRCC3P1']  # the correct answer?? :)
+    """Generate auto-completion suggestions based on the query string.
+
+    Matches query as a case insensitive string at the start of
+    a gene name, and only for the selected species, if requested.
+
+    Connects automatically to the database.
+
+    Parameters
+    ==========
+    query : str
+        A partial query as input by the user, e.g. `brc`
+    species : str
+        The name of the target species, e.g. `homo_sapiens`.
+        Defaults to all species.
+    limit : int
+        The maximum number of suggestions to return, e.g. 10.
+        Defaults to all results.
+
+    Returns
+    =======
+    suggestions : list of str
+        A list of the suggested auto-completions. List is empty of none
+        are found, and will not return more than limit total.
+    """
+    # auto-connect to the database
+    engine, meta, session = get_connection()
+    # autoload inspects the schema and builds the ORM from that
+    # ensures there is never a mismatch between the code and the
+    # target database, and works with multiple database structures.
+    gene_autocomplete = Table('gene_autocomplete', meta, autoload=True,
+                              autoload_with=engine)
+    # shorter names
+    display_label = gene_autocomplete.columns.display_label
+    species_c = gene_autocomplete.columns.species
+
+    c_query = session.query(gene_autocomplete)  # query the database
+    # Case insensitive at beginning of string
+    c_query = c_query.filter(display_label.ilike('{}%'.format(query)))
+    # filter by species only if requested
+    if species is not None:
+        # == may not be case insensitive
+        c_query = c_query.filter(species_c.ilike(species))
+    # only return unique solutions
+    c_query = c_query.distinct()
+
+    if limit is not None:
+        c_query = c_query.limit(limit)
+
+    # convert query to results
+    suggestions = [row.display_label for row in c_query]
+
+    return suggestions
+
--- tests/test_interface.py
+++ tests/test_interface.py
@@ -5,7 +5,7 @@ Define a query and the expected response.
 
 """
 
-# TODO: use a fixed db source that will not change
+# TODO: use a fixed db source that will not change and works offline
 
 from gene_suggest.interface import get_suggestions
 
@@ -27,6 +27,39 @@ def test_live_db():
 
     for result in results:
         assert test_query.lower() in result.lower()
+
+
+def test_query_with_species():
+    """Test that using species gives different results."""
+    # make the same query with and without a species argument
+    # results should be a subset
+    test_query = 'ab'
+    test_species = 'homo_sapiens'
+
+    results_all = get_suggestions(test_query)
+    results_species = get_suggestions(test_query, species=test_species)
+
+    assert all([result in results_all for result in results_species])
+    assert len(results_species) < len(results_all)
+
+
+def test_query_with_limit():
+    """Test that limit will give only up to the maximum number
+    of results"""
+    test_query = 'ag'
+    test_limit = 10
+    test_limit_big = 10**6  # all results
+
+    results_all = get_suggestions(test_query)
+    results_limit = get_suggestions(test_query, limit=test_limit)
+    results_limit_big = get_suggestions(test_query, limit=test_limit_big)
+
+    # with limit is a subset
+    assert len(results_limit) == test_limit
+    assert len(results_limit) < len(results_all)
+    assert all([result in results_all for result in results_limit])
+    # high limit should be the same as no limit
+    assert sorted(results_limit_big) == sorted(results_all)
 
 
 #TODO test failure
